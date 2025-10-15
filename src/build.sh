134c16cdfe1c3ce3f045f27461866a026d39a98a
SeongJae Park <sj@kernel.org>
2025-10-15 16:32:07 -0700

    src: add a build script
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ src/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+bindir=$(dirname "$0")
+hugo_site_dir="$bindir/hugo_site"
+
+if [ ! -d "$hugo_site_dir" ]
+then
+	hugo new site "$hugo_site_dir" --format yaml
+fi
+
+cp "$bindir/hugo.yaml" "$hugo_site_dir/"
+cp -R "$bindir/content" "$bindir/static" "$bindir/assets" "$hugo_site_dir/"
+
+if [ ! -d "$hugo_site_dir/themes/PaperMod" ]
+then
+	git clone https://github.com/adityatelange/hugo-PaperMod \
+		"$hugo_site_dir/themes/PaperMod"
+	git -C "$hugo_site_dir/themes/PaperMod" checkout v8.0
+fi
+
+pushd "$hugo_site_dir"
+hugo
+popd
+
+cp -R "$hugo_site_dir/public"/* "$bindir/../"
